f9d117cee46862ed3e7e4d40f0bb8b26d5115dab
tongqing.w <yurgt@users.noreply.github.com>
2018-07-20 13:54:22 +0800

    增加打包参数判定

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
-#！/bin/sh
+#！/bin/bash
 while getopts "fb" opt
   do
     case $opt in
@@ -13,6 +13,9 @@ while getopts "fb" opt
         cd ./admin
         gulp build
         cd ..
+        ;;
+      ?)
+        echo "请加入有效的打包参数，-f 前后打包， -b 后台打包， -fb 前后台打包"
         ;;
       *)
         echo -e "选项参数错误\n-f: 前台打包\n-b: 后台打包";;
